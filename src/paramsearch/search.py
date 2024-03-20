09e90e0023acbb4f03c634f9da35fd36bf321e68
William Ferreira <wqferr@gmail.com>
2024-03-20 17:53:30 -0300

    Baby's first genetic algorithm

--- src/paramsearch/search.py
+++ src/paramsearch/search.py
@@ -1,47 +1,57 @@
 from pygad import pygad
 import random
 from collections import namedtuple
+from typing import List
 
 SEED = 1997_12_02
 random.seed(SEED)
 
-CHROMOSOME = dict(
-    wOB_AON=0,
-    wOB_PC=1,
-    wAON_OB=2,
-    wAON_PG=3,
-    wPC_AON=4,
-    wDPC_OB=5,
-    # wDPC_PC
-    wOB_LAT_E=6,
-    wOB_LAT_I=7,
-)
-
-RANGES = [
-    {"min": 0.2, "max": 10}, # wOB_AON
-    {"min": 0.2, "max": 10}, # wOB_PC
-    {"min": 0.2, "max": 10}, # wAON_OB
-    {"min": 0.2, "max": 10}, # wAON_PG
-    {"min": 0.2, "max": 10}, # wPC_AON
-    {"min": 0.2, "max": 10}, # wDPC_OB
-    {"min": 0.2, "max": 10}, # wDPC_PC
-    {"min": -8, "max": -0.2}, # wPC_DPC
-    {"min": 0.2, "max": 10}, # wOB_lateral_exc
-    {"min": -8, "max": -0.2}, # wOB_lateral_inhib
+WEIGHT_SIGNS = [
+    +1,
+    +1,
+    +1,
+    +1,
+    +1,
+    +1,
+    +1,
+    -1,
+    +1,
+    -1
 ]
 
-def fitness(chromosome: list[float]) -> float:
-    return 0
+RANGES = []
+for sign in WEIGHT_SIGNS:
+    if sign > 0:
+        RANGES.append({"low": 0.2, "high": 10})
+    else:
+        RANGES.append({"low": -8, "high": -0.2})
+
+def weight_minimizing_component(chromosome: List[float]):
+    total = 0
+    for value in chromosome:
+        # this is meant to be smaller (more negative) the larger
+        # the absolute value of the weights are
+        total -= value ** 2
+    return total
+
+def fitness(ga: pygad.GA, chromosome: List[float], index: int) -> float:
+    total = 0
+    total += weight_minimizing_component(chromosome)
+    return total
 
 def main():
     ga = pygad.GA(
         fitness_func=fitness,
+        num_generations=50,
+        num_parents_mating=2,
         sol_per_pop=20,
         gene_type=float,
-        num_genes=8,
+        num_genes=10,
         gene_space=RANGES,
         random_seed=SEED
     )
+    ga.run()
+    ga.plot_fitness()
 
 if __name__ == "__main__":
     main()
